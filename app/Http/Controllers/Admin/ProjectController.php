01ecafcac93fe0045819419371187bd07dd1b667
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-03 12:09:17 +0200

    fix: move form logic from form to controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -27,7 +27,8 @@ class ProjectController extends Controller
    */
   public function create()
   {
-    return view('admin.projects.form');
+    $editForm = false;
+    return view('admin.projects.form', compact('editForm'));
   }
 
   /**
@@ -68,7 +69,8 @@ class ProjectController extends Controller
    */
   public function edit(Project $project)
   {
-    return view('admin.projects.form', compact('project'));
+    $editForm = true;
+    return view('admin.projects.form', compact('project', 'editForm'));
   }
 
   /**
--- resources/views/admin/projects/form.blade.php
+++ resources/views/admin/projects/form.blade.php
@@ -1,15 +1,4 @@
-@php
-  $editForm = Route::currentRouteName() == 'admin.projects.edit' ? true : false;
-@endphp
-
 @extends('layouts.main')
-
-
-{{-- @section('assets')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-@endsection --}}
 
 @section('maincontent')
   <div class="container my-5">
